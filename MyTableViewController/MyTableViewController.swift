a49a07cb49970dc52a2096d732fca1904131b3ad
Jason Brown <jasonba@smu.edu>
2023-09-21 13:03:46 -0500

    New Commit

---
+++ MyTableViewController/MyTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MyTableViewController.swift
+//  MyTableViewController
+//
+//  Created by jason brown on 18/06/1402 AP.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+    
+    public var models: [(String, (() -> Void))] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].0
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        models[indexPath.row].1()
+    }
+
+}
--- MyTableViewController/ViewController.swift
+++ MyTableViewController/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func didTapButtonOne(){
+        let vc = MyTableViewController()
+        vc.models = [
+            ("first", {print("first")}),
+            ("second", {print("second")}),
+            ("third",{print("third")})
+            ]
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    @IBAction func didTapButtonTwo(){
+        let vc = MyTableViewController()
+        vc.models = [ ]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
-
